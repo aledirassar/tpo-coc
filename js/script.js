cb4d43bf7c16e7b48f23aade3e4d1556e3864262
aledirassar <alejo.dirassar@gmail.com>
2021-05-13 22:21:39 -0300

    primercommit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let botonMenu = document.querySelector(".boton-menu");
+let navMenu = document.querySelector(".nav-menu");
+
+botonMenu.addEventListener("click", function(){
+    navMenu.classList.toggle("boton-menu-visible")
+})
